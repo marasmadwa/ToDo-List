4a55044a4c8f50dc214fed7fdca1175f99b6eb34
marasmadwa <michalkowski.m@gmail.com>
2018-03-29 12:37:13 +0200

    initial commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './js/ToDoList.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <section className='elo'>elo</section>
+    );
+  }
+}
+
