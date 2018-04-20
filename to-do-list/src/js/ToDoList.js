f76a4fda487b611c577324cb5f8098d6276476eb
marasmadwa <michalkowski.m@gmail.com>
2018-04-20 11:30:04 +0200

    Task component added

--- to-do-list/src/js/ToDoList.js
+++ to-do-list/src/js/ToDoList.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
+class Task extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            text: ''
+        }
+    }
 
-export default class ToDoList extends React.Component{
     render(){
+        return(
+            <div>
+                <li>{this.state.text}</li>
+                <button className='removeTask'>Remove task</button>
+                <button className='editTask'>Edit</button>
+                <input type="checkbox"/>
+            </div>
+        )
+    }
+}
+
+export default class ToDoList extends React.Component {
+    render() {
         return (
-            <div className= 'container'>
-              <div className='task'>
-                  <p>To Do:</p>
-                  <div className='profilePicture'></div>
-              </div>
+            <div className='container'>
+                <div className='task'>
+                    <p>To Do:</p>
+                    <div className='profilePicture'></div>
+                </div>
+                <div className="addTask">
+                    <button className='addTaskBtn'>Add task!</button>
+                    <input type="text" placeholder={'What to do?'}/>
+                    <ul>
+                        <Task/>
+                    </ul>
+                </div>
             </div>
         )
     }
